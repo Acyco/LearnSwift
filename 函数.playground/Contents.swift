537a4bb6250b7a3119e004c6d0fc275af55793fb
Acyco <chencongyi@live.com>
2022-05-26 13:37:13 +0800

    函数

---
+++ 函数.playground/Contents.swift
@@ -0,0 +1,217 @@
+import UIKit
+
+// 定义和调用函数
+func greet(person: String) -> String {
+    let greeting = "Hello, " + person + "!"
+    return greeting
+}
+
+print(greet(person: "Acyco"))
+
+print(greet(person: "Anna"))
+
+// 为了简化函数的主体 我们可以将函数返回语句组合成一行
+
+func greetAgain(person: String) -> String {
+    return "Hello Again, " + person + "!"
+}
+
+print(greet(person: "Acyco"))
+
+// 函数的形式参数 和 返回值
+// 无形式参数 的 函数
+
+func sayHelloWorld() -> String {
+    return "Hello, World"
+}
+print(sayHelloWorld())
+
+// 多形式参数的函数
+
+func greet(person:String,alreadyGreeted: Bool) -> String{
+    if alreadyGreeted {
+        return greetAgain(person: person)
+    } else {
+        return greet(person: person)
+    }
+}
+print(greet(person: "Tim", alreadyGreeted: true))
+
+// 无返回值的函数
+func greet2(person: String){
+    print("Hello, \(person)!" )
+}
+greet2(person: "acyco")
+
+func printAndCount(string: String) -> Int {
+    print(string)
+    return string.count
+}
+func printWithoutCounting(string: String) {
+    let _ = printAndCount(string: string)
+}
+printAndCount(string: "hello, world")
+// prints "hello, world" and returns a value of 12
+printWithoutCounting(string: "hello, world")
+// prints "hello, world" but does not return a value
+
+
+// 多返回值的函数
+func minMax(array: [Int]) -> (min: Int, max: Int) {
+    var currentMin = array[0]
+    var currentMax = array[0]
+    for value in array[1..<array.count] {
+        if value < currentMin {
+            currentMin = value
+        } else if value > currentMax {
+            currentMax = value
+        }
+    }
+    return (currentMin, currentMax)
+}
+
+
+let bounds = minMax(array: [8, -6, 2, 109, 3, 71])
+print("min is \(bounds.min) and max is \(bounds.max)")
+// Prints "min is -6 and max is 109"
+
+// 可选元组返回类型  定义返回类型括号外面加个？
+
+
+// 隐式返回的函数 只有单一表达式，可省略那个 return
+
+// 函数实际参数标签和形式参数化
+
+func someFunction(firstParameterName: Int, secondParameterName: Int) {
+    // In the function body, firstParameterName and secondParameterName
+    // refer to the argument values for the first and second parameters.
+}
+someFunction(firstParameterName: 1, secondParameterName: 2)
+
+
+// 指定实际标签
+func greet(person: String, from hometown: String) -> String {
+    return "Hello \(person)!  Glad you could visit from \(hometown)."
+}
+print(greet(person: "Bill", from: "Cupertino"))
+// Prints "Hello Bill!  Glad you could visit from Cupertino."
+
+// 可省略的标签
+func someFunction(_ firstParameterName: Int, secondParameterName: Int) {
+    // In the function body, firstParameterName and secondParameterName
+    // refer to the argument values for the first and second parameters.
+}
+someFunction(1, secondParameterName: 2)
+
+// 默认形式参数值
+func someFunction(parameterWithDefault: Int = 12) {
+    // In the function body, if no arguments are passed to the function
+    // call, the value of parameterWithDefault is 12.
+}
+someFunction(parameterWithDefault: 6) // parameterWithDefault is 6
+someFunction() // parameterWithDefault is 12
+
+// 可变形式参数 （不固定参数的个数）
+func arithmeticMean(_ numbers: Double...) -> Double {
+    var total: Double = 0
+    for number in numbers {
+        total += number
+    }
+    return total / Double(numbers.count)
+}
+arithmeticMean(1, 2, 3, 4, 5)
+// returns 3.0, which is the arithmetic mean of these five numbers
+arithmeticMean(3, 8.25, 18.75)
+// returns 10.0, which is the arithmetic mean of these three numbers
+
+// 输入输出形式参数。（其实就是类似按址传值）
+func swapTwoInts(_ a: inout Int, _ b: inout Int) {
+    let temporaryA = a
+    a = b
+    b = temporaryA
+}
+var someInt = 3
+var anotherInt = 107
+swapTwoInts(&someInt, &anotherInt)
+print("someInt is now \(someInt), and anotherInt is now \(anotherInt)")
+// prints "someInt is now 107, and anotherInt is now 3"
+
+// 函数类型
+func addTwoInts(_ a: Int, _ b: Int) -> Int {
+    return a + b
+}
+func multiplyTwoInts(_ a: Int, _ b: Int) -> Int {
+    return a * b
+}
+
+// 使用函数类型
+var mathFunction:(Int,Int) -> Int = addTwoInts
+print("Result: \(mathFunction(2, 3))")
+
+mathFunction = multiplyTwoInts
+print("Result: \(mathFunction(2, 3))")
+
+let anotherMathFunction = addTwoInts
+
+// 函数类型作为形式参数类型
+func printMathResult(_ mathFunction: (Int, Int) -> Int, _ a: Int, _ b: Int) {
+    print("Result: \(mathFunction(a, b))")
+}
+printMathResult(addTwoInts, 3, 5)
+
+// 函数类型作为返回类型
+
+func stepForward(_ input: Int) -> Int {
+    return input + 1
+}
+func stepBackward(_ input: Int) -> Int {
+    return input - 1
+}
+
+func chooseStepFunction(backwards: Bool) -> (Int) -> Int {
+    return backwards ? stepBackward : stepForward
+}
+
+var currentValue = 3
+let moveNearerToZero = chooseStepFunction(backwards: currentValue > 0)
+// moveNearerToZero now refers to the stepBackward() function
+
+print("Counting to zero:")
+// Counting to zero:
+while currentValue != 0 {
+    print("\(currentValue)... ")
+    currentValue = moveNearerToZero(currentValue)
+}
+print("zero!")
+
+// 内嵌函数
+func chooseStepFunction2(backward: Bool) -> (Int) -> Int {
+    func stepForward(input: Int) -> Int { return input + 1 }
+    func stepBackward(input: Int) -> Int { return input - 1 }
+    return backward ? stepBackward : stepForward
+}
+var currentValue2 = -4
+let moveNearerToZero2 = chooseStepFunction2(backward: currentValue2 > 0)
+// moveNearerToZero now refers to the nested stepForward() function
+while currentValue2 != 0 {
+    print("\(currentValue2)... ")
+    currentValue2 = moveNearerToZero2(currentValue2)
+}
+print("zero!")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
